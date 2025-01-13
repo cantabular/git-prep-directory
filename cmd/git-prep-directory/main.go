790ce20f78526a3b598a267977c587d4db396279
Steven Maude <git@stevenmaude.co.uk>
2025-01-13 17:38:14 +0000

    Update to urfave/cli v2

--- cmd/git-prep-directory/main.go
+++ cmd/git-prep-directory/main.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/sensiblecodeio/git-prep-directory"
 
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
 // CloneTimeout specifies the duration allowed for each individual `git clone`
@@ -30,44 +30,46 @@ func main() {
 	app.Action = actionMain
 
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "url, u",
 			Usage: "URL to clone",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "ref, r",
 			Usage: "ref to checkout",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "destination, d",
 			Usage: "destination dir",
 			Value: "./src",
 		},
-		cli.DurationFlag{
-			Name:   "timeout, t",
-			Usage:  "clone timeout",
-			Value:  CloneTimeout,
-			EnvVar: "GIT_PREP_DIR_TIMEOUT",
+		&cli.DurationFlag{
+			Name:    "timeout, t",
+			Usage:   "clone timeout",
+			Value:   CloneTimeout,
+			EnvVars: []string{"GIT_PREP_DIR_TIMEOUT"},
 		},
 	}
 
 	app.RunAndExitOnError()
 }
 
-func actionMain(c *cli.Context) {
-	if !c.GlobalIsSet("url") || !c.GlobalIsSet("ref") {
+func actionMain(c *cli.Context) error {
+	if !c.IsSet("url") || !c.IsSet("ref") {
 		log.Fatalln("Error: --url and --ref required")
 	}
 
 	where, err := git.PrepBuildDirectory(
-		c.GlobalString("destination"),
-		c.GlobalString("url"),
-		c.GlobalString("ref"),
-		c.GlobalDuration("timeout"),
+		c.String("destination"),
+		c.String("url"),
+		c.String("ref"),
+		c.Duration("timeout"),
 		os.Stderr)
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
 	log.Printf("Checked out %v at %v", where.Name, where.Dir)
 	fmt.Println(where.Dir)
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/sensiblecodeio/git-prep-directory
 go 1.23.4
 
 require (
-	github.com/urfave/cli v1.22.7
+	github.com/urfave/cli/v2 v2.27.5
 	github.com/vaughan0/go-ini v0.0.0-20130923145212-a98ad7ee00ec
 	golang.org/x/sys v0.29.0
 )
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.6 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 )
